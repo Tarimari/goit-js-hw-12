01fe1f3b25bc13b8966f9797177f474d97b50d3c
Tarimari <119706474+Tarimari@users.noreply.github.com>
2024-01-18 12:20:37 +0000

    Deploying to gh-pages from @ Tarimari/goit-js-hw-12@41fd7089ec0b0fbd1ee74100b7ccdb3b6cff53d8 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,7 +1,7 @@
-import{i as c,S as u}from"./assets/vendor-9310f15c.js";(function(){const a=document.createElement("link").relList;if(a&&a.supports&&a.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();require("axios").default;const h=document.querySelector(".submit-form"),n=document.querySelector(".gallery");h.addEventListener("submit",f);function f(r){r.preventDefault(),n.innerHTML='<span class="loader"></span>';const a=r.currentTarget.elements.search.value;m(a).then(s=>{if(s.hits.length===0){c.show({message:"❌ Sorry, there are no images matching your search query. Please try again!"}),n.innerHTML="";return}n.innerHTML=d(s.hits),p()}).catch(s=>console.log(s))}function d(r){return r.map(({webformatURL:a,largeImageURL:s,tags:i,likes:e,views:t,comments:o,downloads:l})=>`
+import{i as h,a as y,S as L}from"./assets/vendor-c145bea9.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const n of o.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerpolicy&&(o.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?o.credentials="include":e.crossorigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function a(e){if(e.ep)return;e.ep=!0;const o=r(e);fetch(e.href,o)}})();let i=1,l;const u=40,b=document.querySelector(".submit-form"),c=document.querySelector(".gallery"),d=document.querySelector(".js-more"),$=document.querySelector(".js-loading");b.addEventListener("submit",q);d.addEventListener("click",_);function _(){i+=1,g(l).then(s=>{c.insertAdjacentHTML("beforeend",f(s.hits)),m()}).catch(s=>console.log(s))}function q(s){s.preventDefault(),$.innerHTML='<span class="loader"></span>',s.currentTarget.elements.search.value!==l&&(c.innerHTML=""),l=s.currentTarget.elements.search.value,g(l).then(t=>{if(t.hits.length===0){h.show({message:"❌ Sorry, there are no images matching your search query. Please try again!"}),c.innerHTML="";return}c.insertAdjacentHTML("beforeend",f(t.hits)),m(),console.log(t),i===1&&(d.hidden=!1),console.log(i),console.log(t.totalHits),console.log(u),i>t.totalHits/u&&(d.hidden=!0,h.show({message:"We're sorry, but you've reached the end of search results."}))}).catch(t=>console.log(t))}function f(s){return s.map(({webformatURL:t,largeImageURL:r,tags:a,likes:e,views:o,comments:n,downloads:p})=>`
         <li class="gallery__item">
-          <a class="gallery__link" href="${s}">
-            <img class="gallery__image" src="${a}" alt="${i}" />
+          <a class="gallery__link" href="${r}">
+            <img class="gallery__image" src="${t}" alt="${a}" />
           </a>
           <ul class="gallery-stats">
             <li class="gallery-stats-item">
@@ -10,17 +10,18 @@ import{i as c,S as u}from"./assets/vendor-9310f15c.js";(function(){const a=docum
             </li>
             <li class="gallery-stats-item">
               <h3 class="stats-head">Views</h3>
-              <p class="stats-indicator">${t}</p>
-            </li>
-            <li class="gallery-stats-item">
-              <h3 class="stats-head">Comments</h3>
               <p class="stats-indicator">${o}</p>
             </li>
             <li class="gallery-stats-item">
+              <h3 class="stats-head">Comments</h3>
+              <p class="stats-indicator">${n}</p>
+            </li>
+            <li class="gallery-stats-item">
               <h3 class="stats-head">Downloads</h3>
-              <p class="stats-indicator">${l}</p>
+              <p class="stats-indicator">${p}</p>
             </li>
           </ul>
         </li>
-   `).join("")}function m(r){return fetch(`https://pixabay.com/api/?key=41768952-3eb5a1819d194e4ebd739434d&q=${r}&image_type=photo&orientation=horizontal&safesearch=true`).then(i=>{if(!i.ok)throw new Error(i.statusText);return i.json()})}function p(){new u(".gallery a",{captionsDelay:250,captionPosition:"bottom"}).refresh()}
+   `).join("")}async function g(s){const t="https://pixabay.com/api/",r="41768952-3eb5a1819d194e4ebd739434d";return(await y.get(`${t}?key=${r}&q=${s}
+  &image_type=photo&orientation=horizontal&safesearch=true&per_page=${u}&page=${i}`)).data}function m(){new L(".gallery a",{captionsDelay:250,captionPosition:"bottom"}).refresh()}
 //# sourceMappingURL=commonHelpers.js.map
